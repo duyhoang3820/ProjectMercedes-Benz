0ed8bcd0fa2b4a043e8c01444ef6aad60125a04a
duyhoang3820 <duyhoang3820@gmail.com>
2021-04-04 22:06:26 +0700

    BTMercedes

---
+++ js/index.js
@@ -0,0 +1,26 @@
+document.getElementById('btnShow').onclick = function() {
+    var more = document.getElementById('showMore');
+    var more1 = document.getElementById('showMore1');
+    var btn = document.getElementById('btnShow');
+    more.style.display = 'block';
+    more1.style.display = 'block';
+    btn.style.display = 'none';
+}
+
+document.getElementById('btnShow1').onclick = function() {
+    var more = document.getElementById('showMore2');
+    var more1 = document.getElementById('showMore3');
+    var btn = document.getElementById('btnShow1');
+    more.style.display = 'block';
+    more1.style.display = 'block';
+    btn.style.display = 'none';
+}
+
+document.getElementById('btnShow2').onclick = function() {
+    var more = document.getElementById('showMore4');
+    var more1 = document.getElementById('showMore5');
+    var btn = document.getElementById('btnShow2');
+    more.style.display = 'block';
+    more1.style.display = 'block';
+    btn.style.display = 'none';
+}
